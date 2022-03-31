84ed4d661ec7572c88f2d9756a2730e1a588c089
William Hutchison <williamh@microsoft.com>
2022-03-31 11:52:18 -0700

    Add SEARCH subfolder
    - Add SEARCH subfolder to __tests__
      - Added sample files
    - Add unit test which to check that searchFolder parameter is handled correctly

--- __tests__/vstest.tests.ts
+++ __tests__/vstest.tests.ts
@@ -1,3 +1,4 @@
+import * as glob from '@actions/glob'
 import {findFilesToUpload} from '../src/search';
 import {getInputs} from '../src/input-helper';
 import {NoFileOptions} from '../src/constants';
@@ -8,11 +9,18 @@ test('test getInputs with no input', async () => {
     expect(inputs.ifNoFilesFound)
 })
 
+test('test findFilesToUpload with empty searchFolder', async () => {
+    var searchFolder = "" as string
+
+    let result = findFilesToUpload(searchFolder)
+    expect((await result).filesToUpload).toBeNull
+})
+
 test('test findFilesToUpload', async () => {
     var searchFolder = process.env['RUNNER_SEARCH'] as string
 
     let result = findFilesToUpload(searchFolder)
-    expect((await result).filesToUpload).toBeGreaterThan(0)
+    expect((await result).filesToUpload).toHaveLength(2)
 })
 
 test('vstest', async () => {
