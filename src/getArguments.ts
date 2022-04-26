0209380ae444a52cd38d9ce0c71ab508b1e12d6f
William Hutchison <williamh@microsoft.com>
2022-04-26 10:48:16 -0700

    Refactor index.ts
    Refactored index.ts into separate modules in order to support unit testing:
    
    - getArguments.ts
    - getTestAssemblies.ts
    - getVsTestPath.ts
    - uploadArtifact.ts

---
+++ src/getArguments.ts
@@ -0,0 +1,53 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as path from 'path';
+import {create, UploadOptions} from '@actions/artifact';
+import {findFilesToUpload} from './search';
+import {getInputs} from './input-helper';
+import {NoFileOptions} from './constants';
+
+export function getArguments(): string {
+  let args = ''
+  let testFiltercriteria = core.getInput('testFiltercriteria')
+  if(testFiltercriteria) {
+    args += `/TestCaseFilter:${testFiltercriteria} `
+  }
+
+  let runSettingsFile = core.getInput('runSettingsFile')
+  if(runSettingsFile) {
+    args += `/Settings:${runSettingsFile} `
+  }
+
+  let pathtoCustomTestAdapters = core.getInput('pathtoCustomTestAdapters')
+  if(pathtoCustomTestAdapters) {
+    args += `/TestAdapterPath:${pathtoCustomTestAdapters} `
+  }
+
+  let runInParallel = core.getInput('runInParallel')
+  if(runInParallel && runInParallel.toUpperCase() === "TRUE") {
+    args += `/Parallel `
+  }
+
+  let runTestsInIsolation = core.getInput('runTestsInIsolation')
+  if(runTestsInIsolation && runTestsInIsolation.toUpperCase() === "TRUE") {
+    args += `/InIsolation `
+  }
+
+  let codeCoverageEnabled = core.getInput('codeCoverageEnabled')
+  if(codeCoverageEnabled && codeCoverageEnabled.toUpperCase() === "TRUE") {
+    args += `/Enablecodecoverage `
+  }
+
+  let platform = core.getInput('platform')
+  if(platform && (platform === "x86" || platform === "x64" || platform === "ARM")) {
+    args += `/Platform:${platform} `
+  }
+
+  let otherConsoleOptions = core.getInput('otherConsoleOptions')
+  if(otherConsoleOptions) {
+    args += `${otherConsoleOptions} `
+  }
+
+  return args
+}
+
