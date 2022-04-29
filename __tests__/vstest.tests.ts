e3b05690bbad0a3aa0add073be5091aa8fb6b08c
William Hutchison <williamh@microsoft.com>
2022-04-29 10:00:40 -0700

    Add code coverage

--- __tests__/vstest.tests.ts
+++ __tests__/vstest.tests.ts
@@ -1,8 +1,66 @@
 import * as glob from '@actions/glob'
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as path from 'path';
 import {findFilesToUpload} from '../src/search';
 import {getInputs} from '../src/input-helper';
 import {NoFileOptions} from '../src/constants';
 import { run } from '../src/index'
+import {uploadArtifact} from '../src/uploadArtifact'
+import {getTestAssemblies} from '../src/getTestAssemblies'
+import {getArguments} from '../src/getArguments'
+import {getVsTestPath} from '../src/getVsTestPath'
+
+describe('vstest Action Unit Tests', ()=>{
+
+    beforeEach(async() => {
+        let workerUri = "https://aka.ms/local-worker-win-x64";
+
+        jest.mock('@actions/core');
+        jest.spyOn(core,'debug');
+        jest.spyOn(core, 'info');
+        jest.spyOn(core, 'getInput');
+        jest.spyOn(core, 'setFailed');
+        jest.spyOn(core, 'warning');
+
+        jest.mock('@actions/exec');
+        jest.spyOn(exec, 'exec');
+
+        jest.mock('path');
+
+
+        jest.mock('../src/uploadArtifact');
+        
+    });
+
+    it("test getArguments with all expected inputs", async () => {
+
+        jest.mock('@actions/core');
+        jest.spyOn(core,'debug');
+        jest.spyOn(core, 'info');
+        jest.spyOn(core, 'getInput');
+
+        // Arrange
+        const coreMock = jest.spyOn(core, 'getInput');
+        
+        coreMock.mockImplementation((arg: string) => 'testFiltercriteria').mockReturnValue('testFilterCriteria');
+        coreMock.mockImplementation((arg: string) => 'runSettingsFile').mockReturnValue('runSettingsFile');
+        coreMock.mockImplementation((arg: string) => 'pathtoCustomTestAdapters').mockReturnValue('pathtoCustomTestAdapters');
+        coreMock.mockImplementation((arg: string) => 'runInParallel').mockReturnValue('true');
+        coreMock.mockImplementation((arg: string) => 'runTestsInIsolation').mockReturnValue('true');
+        coreMock.mockImplementation((arg: string) => 'codeCoverageEnabled').mockReturnValue('true');
+        coreMock.mockImplementation((arg: string) => 'platform').mockReturnValue('x64');
+        coreMock.mockImplementation((arg: string) => 'otherConsoleOptions').mockReturnValue('otherConsoleOptions');
+    
+        // Act
+        var args = getArguments();
+    
+        // Assert
+        expect(args).not.toBeNull();
+    
+    });
+});
+
 
 test('test getInputs with no input', async () => {
     let inputs = getInputs()
--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,7 @@ module.exports = {
       '^.+\\.ts$': 'ts-jest'
     },
     verbose: true,
+    collectCoverage: true,
+    coverageReporters: ["json", "html"],
     setupFilesAfterEnv: ['./jest.setup.js']
   }
